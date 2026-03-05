d22ccec8b09957a22e3c93dee5fa1a67a4e68b8b
Valentin Prugnaud <val@codyval.dev>
2026-03-05 01:57:07 -0800

    refactor: remove unused triggers from organizations and memberships SQL migration
    - Deleted unnecessary triggers related to storage objects and prefixes from the SQL migration file for organizations and memberships, streamlining the migration process.

--- supabase/migrations/20260305094732_create_organizations_and_memberships_table.sql
+++ supabase/migrations/20260305094732_create_organizations_and_memberships_table.sql
@@ -216,15 +216,3 @@ using ((EXISTS ( SELECT 1
 CREATE TRIGGER set_memberships_updated_at BEFORE UPDATE ON public.memberships FOR EACH ROW EXECUTE FUNCTION public.touch();
 
 CREATE TRIGGER set_organizations_updated_at BEFORE UPDATE ON public.organizations FOR EACH ROW EXECUTE FUNCTION public.touch();
-
-CREATE TRIGGER objects_delete_delete_prefix AFTER DELETE ON storage.objects FOR EACH ROW EXECUTE FUNCTION storage.delete_prefix_hierarchy_trigger();
-
-CREATE TRIGGER objects_insert_create_prefix BEFORE INSERT ON storage.objects FOR EACH ROW EXECUTE FUNCTION storage.objects_insert_prefix_trigger();
-
-CREATE TRIGGER objects_update_create_prefix BEFORE UPDATE ON storage.objects FOR EACH ROW WHEN (((new.name <> old.name) OR (new.bucket_id <> old.bucket_id))) EXECUTE FUNCTION storage.objects_update_prefix_trigger();
-
-CREATE TRIGGER prefixes_create_hierarchy BEFORE INSERT ON storage.prefixes FOR EACH ROW WHEN ((pg_trigger_depth() < 1)) EXECUTE FUNCTION storage.prefixes_insert_trigger();
-
-CREATE TRIGGER prefixes_delete_hierarchy AFTER DELETE ON storage.prefixes FOR EACH ROW EXECUTE FUNCTION storage.delete_prefix_hierarchy_trigger();
-
-
